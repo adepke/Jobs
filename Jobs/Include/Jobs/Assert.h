c8c0de74330682a1df5814eb3fdab31cc3aca95f
Andrew Depke <andrewdepke@gmail.com>
2019-04-01 09:42:17 -0600

    Fixed Assert Utility, Support for MSVC 15.9

--- Jobs/Include/Jobs/Assert.h
+++ Jobs/Include/Jobs/Assert.h
@@ -5,25 +5,31 @@
 #include <iostream>  // std::cerr
 #include <exception>  // std::terminate
 
-// TEMP
-#if 0
-
 #if _DEBUG || NDEBUG
-#define JOBS_ASSERT(Expression, ...) \
+  // MSVC supports optional macro stream operator after 15.9
+  #if defined(_MSC_VER) && _MSC_VER <= 1916
+    #define JOBS_ASSERT(Expression, ...) \
 	do \
 	{ \
-		if (!Expression) \
+		if (!(Expression)) \
+		{ \
+			std::cerr << "Assertion \"" << #Expression << "\" failed in " << __FILE__ << ", line " << __LINE__ << ": " << __VA_ARGS__; \
+			std::terminate(); \
+		} \
+	} \
+	while (0)
+  #else
+    #define JOBS_ASSERT(Expression, ...) \
+	do \
+	{ \
+		if (!(Expression)) \
 		{ \
 			std::cerr << "Assertion \"" << #Expression << "\" failed in " << __FILE__ << ", line " << __LINE__ __VA_OPT__(<< ": " << __VA_ARGS__); \
 			std::terminate(); \
 		} \
 	} \
 	while (0)
+  #endif
 #else
-#define JOBS_ASSERT(Expression, ...) do {} while (0)
+  #define JOBS_ASSERT(Expression, ...) do {} while (0)
 #endif
-
-// TEMP
-#endif
-
-#define JOBS_ASSERT(Expression, ...) do {} while (0)
