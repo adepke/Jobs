cf09bcaca007a216540118a0bd081e4e7fccd1e6
Andrew Depke <andrewdepke@gmail.com>
2019-10-27 02:52:20 -0600

    Added Parallel Map

--- Jobs/Include/Jobs/Algorithm.h
+++ Jobs/Include/Jobs/Algorithm.h
@@ -128,4 +128,100 @@ namespace Jobs
 	{
 		Detail::ParallelForInternal<std::true_type>(InManager, std::begin(InContainer), std::end(InContainer), Detail::Empty{}, InFunction);
 	}
+
+	namespace Detail
+	{
+		constexpr auto PayloadSize = 300;  // Optimized for large data. #TODO: Solve per data set to find the optimal batch size.
+
+		template <typename InIterator, typename UnaryOp, typename OutIterator>
+		struct MapPayload
+		{
+			InIterator Input;
+			size_t Count;
+			UnaryOp Operation;
+			OutIterator Output;
+		};
+
+		template <typename Async, typename InIterator, typename OutIterator, typename UnaryOp>
+		void ParallelMapInternal(Manager& InManager, InIterator First, InIterator Last, OutIterator Destination, UnaryOp Operation)
+		{
+			std::shared_ptr<Counter<>> Dependency;
+
+			if constexpr (std::is_same_v<Async, std::false_type>)
+			{
+				Dependency = std::make_shared<Counter<>>(0);
+			}
+
+			const auto Distance = std::distance(First, Last);
+			const size_t Count = Distance / PayloadSize;
+			const size_t Remainder = Distance % PayloadSize;
+
+			std::vector<MapPayload<InIterator, UnaryOp, OutIterator>> Payloads;
+			Payloads.resize(Count);
+
+			// Separate the loops in order to maintain cache locality.
+			for (size_t Iter{ 0 }; Iter < Count; ++Iter)
+			{
+				Payloads[Iter].Input = First + (Iter * PayloadSize);
+				Payloads[Iter].Count = PayloadSize;
+				Payloads[Iter].Operation = Operation;
+				Payloads[Iter].Output = Destination + (Iter * PayloadSize);
+			}
+
+			// Fix the remainder.
+			Payloads[Count - 1].Count += Remainder;
+			
+			for (size_t Iter{ 0 }; Iter < Count; ++Iter)
+			{
+				if constexpr (std::is_same_v<Async, std::true_type>)
+				{
+					InManager.Enqueue(Job{ [](auto Payload)
+						{
+							auto* TypedPayload = reinterpret_cast<MapPayload<InIterator, UnaryOp, OutIterator>*>(Payload);
+
+							std::transform(TypedPayload->Input, TypedPayload->Input + TypedPayload->Count, TypedPayload->Output, TypedPayload->Operation);
+						}, static_cast<void*>(&Payloads[Iter]) });
+				}
+
+				else
+				{
+					InManager.Enqueue(Job{ [](auto Payload)
+						{
+							auto* TypedPayload = reinterpret_cast<MapPayload<InIterator, UnaryOp, OutIterator>*>(Payload);
+
+							std::transform(TypedPayload->Input, TypedPayload->Input + TypedPayload->Count, TypedPayload->Output, TypedPayload->Operation);
+						}, static_cast<void*>(&Payloads[Iter]) }, Dependency);
+				}
+			}
+
+			if constexpr (std::is_same_v<Async, std::false_type>)
+			{
+				Dependency->Wait(0);
+			}
+		}
+	}
+
+	template <typename InIterator, typename OutIterator, typename UnaryOp>
+	inline void ParallelMap(Manager& InManager, InIterator First, InIterator Last, OutIterator Destination, UnaryOp Operation)
+	{
+		return Detail::ParallelMapInternal<std::false_type>(InManager, First, Last, Destination, Operation);
+	}
+
+	template <typename Container, typename OutIterator, typename UnaryOp>
+	inline void ParallelMap(Manager& InManager, Container&& InContainer, OutIterator Destination, UnaryOp Operation)
+	{
+		return Detail::ParallelMapInternal<std::false_type>(InManager, std::begin(InContainer), std::end(InContainer), Destination, Operation);
+	}
+
+	template <typename InIterator, typename OutIterator, typename UnaryOp>
+	inline void ParallelMapAsync(Manager& InManager, InIterator First, InIterator Last, OutIterator Destination, UnaryOp Operation)
+	{
+		return Detail::ParallelMapInternal<std::true_type>(InManager, First, Last, Destination, Operation);
+	}
+
+	template <typename Container, typename OutIterator, typename UnaryOp>
+	inline void ParallelMapAsync(Manager& InManager, Container&& InContainer, OutIterator Destination, UnaryOp Operation)
+	{
+		return Detail::ParallelMapInternal<std::true_type>(InManager, std::begin(InContainer), std::end(InContainer), Destination, Operation);
+	}
 }
