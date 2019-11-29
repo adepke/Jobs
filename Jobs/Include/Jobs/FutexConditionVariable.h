83e183cf913ec6b8d6c2a570d3daecbabe8df1df
Andrew Depke <andrewdepke@gmail.com>
2019-11-29 04:24:49 -0700

    Hide Platform Details from FutexConditionVariable Public Interface

--- Jobs/Include/Jobs/FutexConditionVariable.h
+++ Jobs/Include/Jobs/FutexConditionVariable.h
@@ -2,15 +2,18 @@
 
 #pragma once
 
-#include <chrono>  // std::chrono::duration
-
+namespace Jobs
+{
 #if defined(_WIN32) || defined(_WIN64)
 #define PLATFORM_WINDOWS 1
-#include <Jobs/WindowsMinimal.h>
+	constexpr auto SizeOfUserSpaceLock = 40;
+	constexpr auto SizeOfConditionVariable = 8;
 #else
 #define PLATFORM_POSIX 1
-#include <pthread.h>
+	constexpr auto SizeOfUserSpaceLock = 40;
+	constexpr auto SizeOfConditionVariable = 48;
 #endif
+}
 
 #ifndef PLATFORM_WINDOWS
 #define PLATFORM_WINDOWS 0
@@ -24,101 +27,24 @@ namespace Jobs
 	class FutexConditionVariable
 	{
 	private:
-#if PLATFORM_WINDOWS
-		CRITICAL_SECTION UserSpaceLock;
-		CONDITION_VARIABLE ConditionVariable;
-#else
-		pthread_mutex_t UserSpaceLock = PTHREAD_MUTEX_INITIALIZER;
-		pthread_cond_t ConditionVariable = PTHREAD_COND_INITIALIZER;
-#endif
+		// Opaque housing.
+		unsigned char UserSpaceLock[SizeOfUserSpaceLock];
+		unsigned char ConditionVariable[SizeOfConditionVariable];
 
 	public:
-		inline FutexConditionVariable();
+		FutexConditionVariable();
 		FutexConditionVariable(const FutexConditionVariable&) = delete;
 		FutexConditionVariable(FutexConditionVariable&&) noexcept = delete;
-		inline ~FutexConditionVariable();
+		~FutexConditionVariable();
 
 		FutexConditionVariable& operator=(const FutexConditionVariable&) = delete;
 		FutexConditionVariable& operator=(FutexConditionVariable&&) noexcept = delete;
 
-		inline void Lock();
-		inline void Unlock();
+		void Lock();
+		void Unlock();
 
-		inline void Wait();
-		template <typename Rep, typename Period>
-		bool WaitFor(const std::chrono::duration<Rep, Period>& Timeout);
-		inline void NotifyOne();
-		inline void NotifyAll();
+		void Wait();
+		void NotifyOne();
+		void NotifyAll();
 	};
-
-	FutexConditionVariable::FutexConditionVariable()
-	{
-#if PLATFORM_WINDOWS
-		InitializeCriticalSection(&UserSpaceLock);
-		SetCriticalSectionSpinCount(&UserSpaceLock, 2000);
-
-		InitializeConditionVariable(&ConditionVariable);
-#else
-#endif
-	}
-
-	FutexConditionVariable::~FutexConditionVariable()
-	{
-#if PLATFORM_WINDOWS
-		DeleteCriticalSection(&UserSpaceLock);
-#else
-		pthread_mutex_destroy(&UserSpaceLock);
-		pthread_cond_destroy(&ConditionVariable);
-#endif
-	}
-
-	void FutexConditionVariable::Lock()
-	{
-#if PLATFORM_WINDOWS
-		EnterCriticalSection(&UserSpaceLock);
-#else
-#endif
-	}
-
-	void FutexConditionVariable::Unlock()
-	{
-#if PLATFORM_WINDOWS
-		LeaveCriticalSection(&UserSpaceLock);
-#else
-#endif
-	}
-
-	void FutexConditionVariable::Wait()
-	{
-#if PLATFORM_WINDOWS
-		SleepConditionVariableCS(&ConditionVariable, &UserSpaceLock, INFINITE);
-#else
-#endif
-	}
-
-	template <typename Rep, typename Period>
-	bool FutexConditionVariable::WaitFor(const std::chrono::duration<Rep, Period>& Timeout)
-	{
-#if PLATFORM_WINDOWS
-#else
-#endif
-
-		return false;
-	}
-
-	void FutexConditionVariable::NotifyOne()
-	{
-#if PLATFORM_WINDOWS
-		WakeConditionVariable(&ConditionVariable);
-#else
-#endif
-	}
-
-	void FutexConditionVariable::NotifyAll()
-	{
-#if PLATFORM_WINDOWS
-		WakeAllConditionVariable(&ConditionVariable);
-#else
-#endif
-	}
 }
