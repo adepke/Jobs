820dd959a0f8fa64b23b3232cecfa227687a268f
Andrew Depke <andrewdepke@gmail.com>
2020-11-14 19:03:00 -0700

    Added profiling zones

--- Examples/DirectedAcyclicGraph.cpp
+++ Examples/DirectedAcyclicGraph.cpp
@@ -3,6 +3,7 @@
 #include <Jobs/Manager.h>
 #include <Jobs/Counter.h>
 #include <Jobs/Logging.h>
+#include <Jobs/Profiling.h>
 
 #include <atomic>
 #include <memory>
@@ -59,51 +60,60 @@ int main()
 	LogManager::Get().SetOutputDevice(std::cout);
 
 	Job taskA{ [](auto) {
+		JOBS_SCOPED_STAT("Task A");
 		JOBS_LOG(LogLevel::Log, "A");
 		DoWork(2);
 	} };
 
 	Job taskB{ [](auto) {
+		JOBS_SCOPED_STAT("Task B");
 		JOBS_LOG(LogLevel::Log, "B");
 		DoWork(3);
 	} };
 
 	Job taskC{ [](auto) {
+		JOBS_SCOPED_STAT("Task C");
 		JOBS_LOG(LogLevel::Log, "C");
 		DoWork(5);
 	} };
 
 	Job taskD{ [](auto) {
+		JOBS_SCOPED_STAT("Task D");
 		JOBS_LOG(LogLevel::Log, "D");
 		DoWork(1);
 	} };
 	taskD.AddDependency(counterA);
 
 	Job taskE{ [](auto) {
+		JOBS_SCOPED_STAT("Task E");
 		JOBS_LOG(LogLevel::Log, "E");
 		DoWork(7);
 	} };
 	taskE.AddDependency(counterA);
 
 	Job taskF{ [](auto) {
+		JOBS_SCOPED_STAT("Task F");
 		JOBS_LOG(LogLevel::Log, "F");
 		DoWork(3);
 	} };
 	taskF.AddDependency(counterB);
 
 	Job taskG{ [](auto) {
+		JOBS_SCOPED_STAT("Task G");
 		JOBS_LOG(LogLevel::Log, "G");
 		DoWork(5);
 	} };
 	taskG.AddDependency(counterD);
 
 	Job taskH{ [](auto) {
+		JOBS_SCOPED_STAT("Task H");
 		JOBS_LOG(LogLevel::Log, "H");
 		DoWork(1);
 	} };
 	taskH.AddDependency(counterD);
 
 	Job taskI{ [](auto) {
+		JOBS_SCOPED_STAT("Task I");
 		JOBS_LOG(LogLevel::Log, "I");
 		DoWork(3);
 	} };
@@ -111,6 +121,7 @@ int main()
 	taskI.AddDependency(counterE);
 
 	Job taskJ{ [](auto) {
+		JOBS_SCOPED_STAT("Task J");
 		JOBS_LOG(LogLevel::Log, "J");
 		DoWork(2);
 	} };
@@ -119,6 +130,7 @@ int main()
 	taskJ.AddDependency(counterI);
 
 	Job taskK{ [](auto) {
+		JOBS_SCOPED_STAT("Task K");
 		JOBS_LOG(LogLevel::Log, "K");
 		DoWork(5);
 	} };
@@ -126,6 +138,7 @@ int main()
 	taskK.AddDependency(counterF);
 
 	Job taskL{ [](auto) {
+		JOBS_SCOPED_STAT("Task L");
 		JOBS_LOG(LogLevel::Log, "L");
 		DoWork(3);
 	} };
@@ -133,6 +146,7 @@ int main()
 	taskL.AddDependency(counterC);
 
 	Job taskM{ [](auto) {
+		JOBS_SCOPED_STAT("Task M");
 		JOBS_LOG(LogLevel::Log, "M");
 		DoWork(1);
 	} };
@@ -144,19 +158,23 @@ int main()
 	// might test their dependencies and move on, before their dependent job
 	// has even had a chance to be enqueued on the main thread.
 
-	manager.Enqueue(taskA, counterA);
-	manager.Enqueue(taskB, counterB);
-	manager.Enqueue(taskC, counterC);
-	manager.Enqueue(taskD, counterD);
-	manager.Enqueue(taskE, counterE);
-	manager.Enqueue(taskF, counterF);
-	manager.Enqueue(taskG, counterG);
-	manager.Enqueue(taskH, counterH);
-	manager.Enqueue(taskI, counterI);
-	manager.Enqueue(taskJ, counterJ);
-	manager.Enqueue(taskK, counterK);
-	manager.Enqueue(taskL, counterL);
-	manager.Enqueue(taskM, counterM);
+	{
+		JOBS_SCOPED_STAT("DAG Enqueue");
+
+		manager.Enqueue(taskA, counterA);
+		manager.Enqueue(taskB, counterB);
+		manager.Enqueue(taskC, counterC);
+		manager.Enqueue(taskD, counterD);
+		manager.Enqueue(taskE, counterE);
+		manager.Enqueue(taskF, counterF);
+		manager.Enqueue(taskG, counterG);
+		manager.Enqueue(taskH, counterH);
+		manager.Enqueue(taskI, counterI);
+		manager.Enqueue(taskJ, counterJ);
+		manager.Enqueue(taskK, counterK);
+		manager.Enqueue(taskL, counterL);
+		manager.Enqueue(taskM, counterM);
+	}
 
 	// Wait for the last task, M, to finish.
 	counterM->Wait(0);
