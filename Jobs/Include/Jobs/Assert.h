b12fecb73714d81e244647b103942f463a7c2d20
Andrew Depke <andrewdepke@gmail.com>
2020-11-19 05:11:33 -0700

    Removed __VA_OPT__ path

--- Jobs/Include/Jobs/Assert.h
+++ Jobs/Include/Jobs/Assert.h
@@ -5,10 +5,8 @@
 #include <iostream>  // std::cerr
 #include <exception>  // std::terminate
 
-#if _DEBUG || NDEBUG
-  // MSVC doesn't yet support __VA_OPT__, see https://en.cppreference.com/w/cpp/compiler_support
-  #if defined(_MSC_VER) /*&& _MSC_VER <= 1916*/
-    #define JOBS_ASSERT(Expression, ...) \
+#if DEBUG || _DEBUG
+  #define JOBS_ASSERT(Expression, ...) \
 	do \
 	{ \
 		if (!(Expression)) \
@@ -18,18 +16,6 @@
 		} \
 	} \
 	while (0)
-  #else
-    #define JOBS_ASSERT(Expression, ...) \
-	do \
-	{ \
-		if (!(Expression)) \
-		{ \
-			std::cerr << "Assertion \"" << #Expression << "\" failed in " << __FILE__ << ", line " << __LINE__ __VA_OPT__(<< ": " << __VA_ARGS__); \
-			std::terminate(); \
-		} \
-	} \
-	while (0)
-  #endif
 #else
   #define JOBS_ASSERT(Expression, ...) do {} while (0)
 #endif
