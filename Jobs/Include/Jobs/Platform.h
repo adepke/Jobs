24659b9e00a97e279dc09b000175453a509372ee
Andrew Depke <andrewdepke@gmail.com>
2020-12-07 17:30:48 -0700

    Fixed redefinition warnings

--- Jobs/Include/Jobs/Platform.h
+++ Jobs/Include/Jobs/Platform.h
@@ -2,11 +2,15 @@
 
 #pragma once
 
-#define JOBS_PLATFORM_WINDOWS 0
-#define JOBS_PLATFORM_POSIX 0
-
 #ifdef _WIN32
   #define JOBS_PLATFORM_WINDOWS 1
 #else
   #define JOBS_PLATFORM_POSIX 1
+#endif
+
+#ifndef JOBS_PLATFORM_WINDOWS
+  #define JOBS_PLATFORM_WINDOWS 0
+#endif
+#ifndef JOBS_PLATFORM_POSIX
+  #define JOBS_PLATFORM_POSIX 0
 #endif
